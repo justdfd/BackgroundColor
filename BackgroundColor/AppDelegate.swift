e07aee01a3f0c2614672f25b670c04f2e7057707
dfd <dfd@justdfd.com>
2019-05-28 15:37:49 -0400

    Initial commit.

--- BackgroundColor/AppDelegate.swift
+++ BackgroundColor/AppDelegate.swift
@@ -12,10 +12,17 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    var navigationController: UINavigationController?
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)        
+        if let window = window{
+            let reportsVC = ReportsVC()
+            navigationController = UINavigationController(rootViewController: reportsVC)
+            window.rootViewController = navigationController
+            window.makeKeyAndVisible()
+        }
         return true
     }
 
--- BackgroundColor/ViewController.swift
+++ BackgroundColor/ViewController.swift
@@ -8,13 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ReportsVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    override func loadView() {
+        view = MyView()
+    }
+}
+class MyView:UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = UIColor.green
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
 
